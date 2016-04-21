0dfbec988b91660a5f04860574bcb53d9c6d0afa
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2016-04-21 11:48:08 +0200

    allow to repair shared libraries different from Python extension modules bundled in the wheel

--- auditwheel/wheel_abi.py
+++ auditwheel/wheel_abi.py
@@ -39,74 +39,19 @@ def get_wheel_elfdata(wheel_fn: str):
         shared_libraries_in_purelib = []
 
         for fn, elf in elf_file_filter(ctx.iter_files()):
+            is_py_ext, py_ver = elf_is_python_extension(fn, elf)
 
-            # Check for invalid binary wheel format: no shared library should
-            # be found in purelib
-            so_path_split = fn.split(os.sep)
+            log.info('processing: %s', fn)
+            elftree = lddtree(fn)
+            full_elftree[fn] = elftree
+            for key, value in elf_find_versioned_symbols(elf):
+                versioned_symbols[key].add(value)
 
-            # If this is in purelib, add it to the list of shared libraries in
-            # purelib
-            if 'purelib' in so_path_split:
-                shared_libraries_in_purelib.append(so_path_split[-1])
-
-            # If at least one shared library exists in purelib, this is going
-            # to fail and there's no need to do further checks
-            if not shared_libraries_in_purelib:
-                log.debug('processing: %s', fn)
-                elftree = lddtree(fn)
-
-                for key, value in elf_find_versioned_symbols(elf):
-                    log.debug('key %s, value %s', key, value)
-                    versioned_symbols[key].add(value)
-
-                is_py_ext, py_ver = elf_is_python_extension(fn, elf)
-
-                # If the ELF is a Python extention, we definitely need to
-                # include its external dependencies.
-                if is_py_ext:
-                    full_elftree[fn] = elftree
-                    uses_PyFPE_jbuf |= elf_references_PyFPE_jbuf(elf)
-                    if py_ver == 2:
-                        uses_ucs2_symbols |= any(
-                            True for _ in elf_find_ucs2_symbols(elf))
-                    full_external_refs[fn] = lddtree_external_references(
-                        elftree,
-                        ctx.path)
-                else:
-                    # If the ELF is not a Python extension, it might be
-                    # included in the wheel already because auditwheel repair
-                    # vendored it, so we will check whether we should include
-                    # its internal references later.
-                    nonpy_elftree[fn] = elftree
-
-        # If at least one shared library exists in purelib, raise an error
-        if shared_libraries_in_purelib:
-            raise RuntimeError(
-                (
-                    'Invalid binary wheel, found the following shared '
-                    'library/libraries in purelib folder:\n'
-                    '\t%s\n'
-                    'The wheel has to be platlib compliant in order to be '
-                    'repaired by auditwheel.'
-                ) % '\n\t'.join(shared_libraries_in_purelib)
-            )
-
-        # Get a list of all external libraries needed by ELFs in the wheel.
-        needed_libs = {
-            lib
-            for elf in itertools.chain(full_elftree.values(),
-                                       nonpy_elftree.values())
-            for lib in elf['needed']
-        }
-
-        for fn in nonpy_elftree.keys():
-            # If a non-pyextension ELF file is not needed by something else
-            # inside the wheel, then it was not checked by the logic above and
-            # we should walk its elftree.
-            if basename(fn) not in needed_libs:
-                full_elftree[fn] = nonpy_elftree[fn]
-                full_external_refs[fn] = lddtree_external_references(
-                    nonpy_elftree[fn], ctx.path)
+            if is_py_ext and py_ver == 2:
+                uses_ucs2_symbols |= any(
+                    True for _ in elf_find_ucs2_symbols(elf))
+            full_external_refs[fn] = lddtree_external_references(elftree,
+                                                                 ctx.path)
 
     log.debug(json.dumps(full_elftree, indent=4))
 
